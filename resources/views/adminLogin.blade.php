d893829671ebd28623e2d340002b24a6d2eec766
Erico Weber <ericoweber@hotmail.com>
2020-06-04 10:10:17 -0300

    inicio login personaltrainer

---
+++ resources/views/adminLogin.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Academia')
+
+@section('content_header')
+    <h1>Academia do Maroto</h1>
+@stop
+
+@section('content')
+    <p>Painel Principal do Funcionário</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
